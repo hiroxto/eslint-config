574ba04a082e2acbb5be9625e1bdcc262f1a6295
hiroxto <hiroxto@gmail.com>
2021-09-15 23:02:09 +0900

    メインの設定を作成

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:promise/recommended',
+    'standard',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    semi: ['error', 'always'],
+    'no-console': 'off',
+    'no-new': 'off',
+    indent: ['error', 2],
+    'quote-props': ['error', 'as-needed'],
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    'promise/catch-or-return': ['error', { allowThen: true, terminationMethod: ['catch', 'asCallback', 'finally'] }],
+  },
+};
